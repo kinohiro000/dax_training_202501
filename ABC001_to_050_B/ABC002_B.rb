3fe9c81bbf613c5965e11d19f2f0d1ba78f18f34
kinohiro000 <kinohiro000@gmail.com>
2025-02-11 17:12:51 +0900

    ABC002_B_fixed

--- ABC001_to_050_B/ABC002_B.rb
+++ ABC001_to_050_B/ABC002_B.rb
@@ -15,21 +15,3 @@ w.length.times do |i|
     flg = 0
 end
 puts output
-
-
-あなたはRubyの扱いに慣れている、ソフトウェア開発者です
-次のコードを、以下に示す観点でレビューし、ブラッシュアップさせてください
-
-- チーム開発をする上で、他の人が読みやすいかどうか
-- 処理速度は速いか
-- Rubyの長所を活かせているか
-
-## コード
-# ~~
-
-# input = gets.chomp
-# vowels = %w[a i u e o]
-
-# output = input.chars.reject { |char| vowels.include?(char) }.join
-
-# puts output
